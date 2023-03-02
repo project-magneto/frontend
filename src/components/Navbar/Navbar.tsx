828dd19ae821e250bec7d73cb200402fce8e42d5
Stiven Mosquera <mosquerast3.dev@gmail.com>
2023-03-02 12:15:04 -0500

    chore: navbar links implemented

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './styles/Navbar.css'
 
 export interface NavbarInterface {}
 
 const Navbar: React.FC<NavbarInterface> = () => (
     <nav className="navbar">
-        <a className="navbar-brand" href="#">Library</a>
+        <Link to={'/loads'} className="nav-link">
+            Library
+        </Link>
         <ul className="navbar-nav">
             <li className="nav-item">
-                <a className="nav-link" href="#">Home</a>
+                <Link to={'/loads'} className="nav-link">
+                    Loads
+                </Link>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
+                <Link to={'/books'} className="nav-link">
+                    Books
+                </Link>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="#">Contact</a>
+                <Link to={'/book-copies'} className="nav-link">
+                    Book copies
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link to={'/users'} className="nav-link">
+                    Users
+                </Link>
             </li>
         </ul>
     </nav>
